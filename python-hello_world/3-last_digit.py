6d72847abcedc9e00b332c760cbcafa3e66f9f09
sirisenju <amrohore4real@gmail.com>
2023-07-19 18:48:49 -0700

    modified code

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,6 +5,6 @@ new_num = number - (10 * int(number / 10))
 if new_num > 5:
     print("Last digit of", number, "is", new_num, "and is greater than 5")
 elif new_num < 6:
-    print("Last digit of", number, "is", new_num, "and is less than 6 and not 0")
-else:
     print("Last digit of", number, "is", new_num, "and is 0")
+else:
+    print("Last digit of", number, "is", new_num, "and is less than 6 and not 0")
