7fc8972ae1c09ce0e9982537b6fd190e073fa829
sirisenju <amrohore4real@gmail.com>
2023-07-23 14:18:29 -0700

    modified code

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -3,7 +3,7 @@ def fibonacci_sequence(n):
     if n == 0:
         return []
     elif n == 1:
-        return [0, 1]
+        return [0]
     fibonacci_sequence = [0, 1]
     while len(fibonacci_sequence) < n:
         next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
