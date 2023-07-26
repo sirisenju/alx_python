40b4ee24c7564eabce5b4c06dab2aaf4d84a1ee9
sirisenju <amrohore4real@gmail.com>
2023-07-26 09:45:06 -0700

    modified code

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,12 +4,12 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 0:
-        print("{} arguments.".format(len(argv)))
+        print("{} arguments.".format(0))
     elif len(argv) == 1:
         print("{} arguments.".format(len(argv)))
         print("{}: {}".format(len(argv), argv[0]))
     else: 
         print("{} arguments.".format(len(argv)))
-        for i in range(0, len(argv)):
+        for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
     
