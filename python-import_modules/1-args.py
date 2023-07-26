67e7a16cb788d396e79174306f97aaff7c3d0b87
sirisenju <amrohore4real@gmail.com>
2023-07-26 10:07:46 -0700

    modified code

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if num_of_args == 0:
         print("{} arguments.".format(0))
     elif num_of_args == 1:
-        print("{} argument.".format(num_of_args))
+        print("{} argument:".format(num_of_args))
         print("{}: {}".format(num_of_args, argv[1]))
     else: 
-        print("{} arguments.".format(num_of_args))
+        print("{} arguments:".format(num_of_args))
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
