c56e005f4ae17db2205e7bef3de3851cc1f48007
sirisenju <amrohore4real@gmail.com>
2023-07-26 09:56:58 -0700

    modified code

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,15 +1,16 @@
 # !/usr/bin/python3
 from sys import argv
 
+num_of_args = len(argv) - 1
 
 if __name__ == "__main__":
-    if len(argv) == 0:
+    if num_of_args == 0:
         print("{} arguments.".format(0))
-    elif len(argv) == 1:
-        print("{} argument.".format(len(argv)))
-        print("{}: {}".format(len(argv), argv[1]))
+    elif num_of_args == 1:
+        print("{} argument.".format(num_of_args))
+        print("{}: {}".format(num_of_args, num_of_args[1]))
     else: 
-        print("{} arguments.".format(len(argv)))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments.".format(num_of_args))
+        for i in range(1, num_of_args):
+            print("{}: {}".format(i, num_of_args[i]))
     
