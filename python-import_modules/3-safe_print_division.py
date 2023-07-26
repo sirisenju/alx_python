ff90d2752a13c6ae1471c9825de8f64962cfeb63
sirisenju <amrohore4real@gmail.com>
2023-07-26 15:57:35 -0700

    modified code

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,13 +2,14 @@
 
 def safe_print_division(a, b):
     try:
-         a / b
+        result = a / b
     except ZeroDivisionError:
+        print("Inside result: {}".format(None))
         return None
     finally:
-        print("Inside result: {}".format(a / b))
+        print("Inside result: {}".format(result))
 
-    return a / b
+    
 
 a = 9
 b = 3
