791b63ea40b257e59deb485d2b0ee84a9a1c05af
sirisenju <amrohore4real@gmail.com>
2023-07-26 09:41:20 -0700

    modified code

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("{}: {}".format(len(argv), argv[0]))
     else: 
         print("{} arguments.".format(len(argv)))
-        for i in range(1, len(argv)):
+        for i in range(1, len(argv)-1):
             print("{}: {}".format(i, argv[i]))
     
