db9a4657cab06307110ba39014453c49994c365d
sirisenju <amrohore4real@gmail.com>
2023-07-26 10:02:00 -0700

    modified code

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(0))
     elif num_of_args == 1:
         print("{} argument.".format(num_of_args))
-        print("{}: {}".format(num_of_args, num_of_args[1]))
+        print("{}: {}".format(num_of_args, argv[1]))
     else: 
         print("{} arguments.".format(num_of_args))
         for i in range(1, num_of_args):
