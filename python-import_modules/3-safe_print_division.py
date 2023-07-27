fb16f281edb3417f819ee570f1664984c4bf6c6e
sirisenju <amrohore4real@gmail.com>
2023-07-27 15:09:44 -0700

    modified code

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,15 +6,11 @@ def safe_print_division(a, b):
    except ZeroDivisionError:
       print("Inside result: {}".format(None))
       print("{:d} / {:d} = {}".format(a, b, None))
-   else:
-      print("{}".format(result))
    finally:
       return "Inside result: {}".format()
    
-
-a = 9
-b = 3
-
+   return result
+   
 
 if __name__ == "__main__":
-   safe_print_division(a, b)
+   safe_print_division()
