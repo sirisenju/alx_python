036177935fafed696944dd061ed61725a22d9be3
sirisenju <amrohore4real@gmail.com>
2023-07-31 16:23:46 -0700

    initial commit / return no string

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+
+def no_c(my_string):
+    for i in my_string:
+        if i == "c" or i == "C":
+            my_string.pop(i)
+            return my_string
+    return my_string
+
+
+if __name__ == "__main__":
+    no_c()
