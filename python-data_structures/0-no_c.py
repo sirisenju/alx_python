fd66a9838e1a767a730f43497796a3617bb6c625
sirisenju <amrohore4real@gmail.com>
2023-07-31 16:54:11 -0700

    modified code

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -6,8 +6,7 @@ def no_c(my_string):
     for i in arr:
         if i == "c" or i == "C":
             arr.pop(i)
-            new_string.join(arr)
-            return "{}".format(new_string)
+            return "{}".format(arr)
     return "{}".format(my_string)
 
 if __name__ == "__main__":
