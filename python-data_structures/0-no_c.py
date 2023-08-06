e1f649fe66041fb25a6e9c88bf06de21df5a0a5b
sirisenju <amrohore4real@gmail.com>
2023-08-06 16:53:44 -0700

    modified code

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,13 +1,9 @@
 # !/usr/bin/python3
 
 def no_c(my_string):
-    arr = [my_string]
-    new_string = " "
-    for i in arr:
-        if i == "c" or i == "C":
-            arr.pop(i)
-            return "{}".format(arr)
-    return "{}".format(my_string)
+    lst = [i for i in my_string.lower() if i != "c"]
+    newStr = "".join(lst)
+    return newStr
 
 if __name__ == "__main__":
     no_c() 
