53b9ef7704f56e453fc044745803073772eaf88d
sirisenju <amrohore4real@gmail.com>
2023-08-06 17:46:11 -0700

    print integer of matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for num in row:
+            print(num, end=' ')
+        print()
+
+
+if __name__ == "__main__":
+    print_matrix_integer(matrix=[[]])
