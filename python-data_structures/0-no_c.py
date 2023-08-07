4dd7fb67f63d1aa9308169227277c046de0e7c16
sirisenju <amrohore4real@gmail.com>
2023-08-06 17:02:40 -0700

    modified code

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,9 +1,9 @@
 # !/usr/bin/python3
 
 def no_c(my_string):
-    lst = [i for i in my_string.lower() if i != "c"]
+    lst = [i for i in my_string if i != "c" and "C"]
     newStr = "".join(lst)
-    return newStr.capitalize()
+    return newStr
 
 if __name__ == "__main__":
     no_c() 
