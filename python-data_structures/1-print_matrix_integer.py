8a3ea0f1f239d8a410e60a5e56d269171cfa7c1c
sirisenju <amrohore4real@gmail.com>
2023-08-11 03:29:00 -0700

    modified code

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,8 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
-        
+            print("{:d}".format(num), end=" ") 
+    print()
 
 
 if __name__ == "__main__":
