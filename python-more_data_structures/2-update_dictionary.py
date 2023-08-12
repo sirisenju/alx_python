711812fef876542fad02fc368822ace58e4ae0d9
sirisenju <amrohore4real@gmail.com>
2023-08-12 16:05:07 -0700

    modified code

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,6 +1,9 @@
 # !/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary = {str(key): value}
-    update_dictionary.update(a_dictionary)
-    return update_dictionary
+    new_dictionary = {str(key): value}
+    a_dictionary.update(new_dictionary)
+    return a_dictionary
+
+if __name__ == "__main__":
+    update_dictionary()
