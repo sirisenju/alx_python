a72d80a0d31e24bdf2178e2ecc1e03fbc4acc23f
sirisenju <amrohore4real@gmail.com>
2023-08-12 17:26:55 -0700

    modified code

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,6 +1,4 @@
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda x: x * number, my_list))
-    return new_list
-
+    return list(map(lambda x: x * number, my_list))
 if __name__ == "__main__":
     multiply_list_map()
