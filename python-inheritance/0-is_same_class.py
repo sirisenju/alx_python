de26adbb2e3fb71a5b08b42992e7ba7d75ad82fa
sirisenju <amrohore4real@gmail.com>
2023-08-20 05:07:05 -0700

    modified code

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,23 +1,6 @@
 # !/usr/bin/python3
 
-class AlxClass:
-    """The summary line for a class docstring should fit on one line.
-
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
-
-    Attributes:
-        attr1 (str): Description of `attr1`.
-
-    """
-    pass
-
-
 def is_same_class(obj, a_class):
+    """ returns true is obj is instance of a class """
     return type(obj) is a_class
 
