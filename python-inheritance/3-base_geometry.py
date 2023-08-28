4cf9cd3bc848fcc5ad5695ca9461c68082d7891d
sirisenju <amrohore4real@gmail.com>
2023-08-28 15:30:16 -0700

    modified code

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,6 +3,16 @@
     empty class BaseGeometry
 """
 
+
 class BaseGeometry:
     """ BaseGeometry class empty """
-    pass
+
+    def __dir__(self):
+        """
+        Get the list of attributes from the parent class (object class)
+        """
+        attributes = super().__dir__()
+
+        """ Exclude __init_subclass__ from the list of attributes
+        """
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
