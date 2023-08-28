b794df1b70508b6b9123372d017d37e596c968b7
sirisenju <amrohore4real@gmail.com>
2023-08-28 15:45:51 -0700

    modified code

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,83 +1,44 @@
-#!/usr/bin/python3
-
-"""
-This module defines a function to check if object is exactly an instance of a specified class.
-
-Function:
-    is_same_class(obj, a_class): Checks if object is exactly an instance of the specified class.
-
-"""
-
-
 class BaseGeometry:
-    """
-    This is a class representing the base geometry.
-
-    It can be used as a base class to create other geometry-related classes.
-
-    Attributes:
-        None
-
-    Methods:
-        area(self):Raises an Exception with the message "area() is not implemented".
-        integer_validator(self, name, value): Validates the value as an integer and raises exceptions if invalid.
-    """
-
     def area(self):
-        """
-        Raises an Exception with the message "area() is not implemented".
-
-        This method needs to be implemented in the subclass to calculate
-        the area of the specific geometry.
-        """
-        raise Exception("area() is not implemented")
+        """Raises a NotImplementedError because subclasses must implement the 'area' method."""
+        raise NotImplementedError(
+            "Subclasses must implement the 'area' method.")
 
     def integer_validator(self, name, value):
         """
-        Validates the value as an integer and raises exceptions if invalid.
+        Validates that a value is an integer greater than 0.
 
-        Parameters:
-            name (str): The name of the value to be validated.
-            value: The value to be validated.
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to validate.
 
         Raises:
-            TypeError, if the value is not an integer.
-            ValueError, if the value is <= 0.
-
-        Returns:
-            None
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not greater than 0.
         """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
-    """
-    This class represents a Rectangle.
-
-    It inherits from the BaseGeometry class.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
-
     def __init__(self, width, height):
         """
-        Initialize the Rectangle with width and height.
+        Initializes a Rectangle instance with given width and height.
 
-        Parameters:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError, if width or height is not an integer.
-            ValueError, ff width or height is <=.
+        Args:
+            width (int): The width of the rectangle (must be a positive integer).
+            height (int): The height of the rectangle (must be a positive integer).
         """
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
 
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
+    def area(self):
+        """
+        Computes the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
